50d05c702016e5ac0807e2d0daf01f49a1dc136a
shariq-tintash <shariq.rashid@tintash.com>
2022-08-20 19:45:50 +0500

    basic setup

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
